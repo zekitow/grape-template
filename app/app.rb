fb5018cbf1365cf16a9e18c71601ecbce7997a99
José Ribeiro <zeca@digup.io>
2017-07-05 13:58:56 -0300

    Added basic auth.

--- app/app.rb
+++ app/app.rb
@@ -7,6 +7,10 @@ module APIs
     rescue_from Grape::Exceptions::ValidationErrors, with: -> (e) { render_validation_error!(e) }
     rescue_from ActiveRecord::RecordNotFound,        with: -> { render_not_found! }
 
+    http_basic do |username, password|
+      ($auth_username == username and $auth_password == password)
+    end
+
     mount APIs::Todos
   end
 end
