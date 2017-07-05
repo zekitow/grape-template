23077cea1e7a13b89aa9a0c1ee010e7cdc7d37e8
José Ribeiro <zeca@digup.io>
2017-07-05 14:16:11 -0300

    Revert "Added basic auth."
    This reverts commit fb5018cbf1365cf16a9e18c71601ecbce7997a99.

--- config/env.rb
+++ config/env.rb
@@ -1,16 +1,10 @@
 require 'bundler'
-require 'yaml'
 
 # Load environment settings
 env = ENV['RACK_ENV'] ? ENV['RACK_ENV'].to_sym : :development
 Bundler.require(:default, env)
 
 OTR::ActiveRecord.configure_from_file! "./config/database.yml"
-
-
-config    = YAML.load_file('./config/config.yml')
-$auth_username = config[env.to_s]['api']['username']
-$auth_password = config[env.to_s]['api']['password']
 
 # Load application
 Dir.glob('./app/apis/{params}/*.rb').each { |file| require file }
